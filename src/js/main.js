a248d42adc5140a93ec80f350e58f95a0818435e
v-lavs <lavskavanda@gmail.com>
2020-11-10 19:34:35 +0200

    added page main, desktop

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+//= include ../../node_modules/jquery/dist/jquery.js ;
+
+
+// CUSTOM SCRIPTS
+
+$(document).ready(function () {
+
+    //MOBILE MENU
+    var nav = $('.header__nav');
+
+    $('.burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.btn-close, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+
+    //HEADER SCROLL
+
+    function onHeaderScrol() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 40) {
+            jQuery(".header").addClass('header_active');
+        } else {
+            jQuery(".header").removeClass('header_active');
+        }
+    }
+    $(document).on('scroll', function () {
+        onHeaderScrol()
+    });
+
+// VIDEO PLAY
+
+    $('#playButton').click(function(){
+        $('video')[0].play();
+        $('#playButton').hide();
+    })
+});
